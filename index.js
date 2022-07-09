3608f77e58d185839d02e1569582422a80c93f07
kimduc1409 <94879200+kimduc1409@users.noreply.github.com>
2022-07-09 18:40:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function open_signup(){
+    var modal = document.querySelector('#modal');
+   if(modal.style.display==''||modal.style.display=='none')
+    modal.style.display='flex';
+    else modal.style.display='none';
+}
